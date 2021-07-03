6b007e01ba5db79986619e1d67cfa415d11386e7
Vinay-JA <72953657+Vinay-JA@users.noreply.github.com>
2021-07-02 17:14:02 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preload(){
+
+}
+function setup(){
+    canvas = createCanvas(600,500);
+    canvas.position(100,250);
+    video = createCapture(VIDEO);
+    video.hide();
+    tint_color = "";
+}
+function draw(){
+    image(video, 0,0,600,500);
+    tint(tint_color);
+}
+function take_snapshot(){
+    save('photo.png');
+}
+function apply_filter(){
+    tint_color = document.getElementById("color_name").value;
+}
